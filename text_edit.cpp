19e61161b7bec8da33613a9fb2c5e9892b4519e3
Sergio Rodriguez <sergio.a.rodriguez@du.edu>
2025-01-09 09:15:57 -0700

    Cleaned up the dir

--- text_edit.cpp
+++ text_edit.cpp
@@ -1,33 +1,54 @@
-#include <iostream>
-#include <string>
-#include <fstream>
+
+#include <termios.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
 
 
-int main(int argc, char *argv[]){
+struct termios orig_termios;
+void disableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios); // set normal terminal settings back
+}
 
-    // Open a file via the standard input arguments
+void enableRawMode() {
+    tcgetattr(STDIN_FILENO, &orig_termios); // store curent terminal settings to struct
+    atexit(disableRawMode); //disable raw mode at exit
 
-    std::ifstream file(argv[1]);
-    if(!file){ // Check if file was opened successfully
-        std::cerr << "Error opening file" << std::endl;
-        return 1;
+    struct termios raw = orig_termios;
+
+    /*
+    ECHO turning off echo mode disables terminal printing
+    ICANON turning off canocial mode
+    ISIG turns off ctrl + x and ctrl + z
+    IXON turns off ctrl + x and ctrl + q
+    IEXTEN turns off ctrl + v
+    ICRNL turns off ctrl + m
+    OPOST turns off automoatic newline and carriage return requiring manual \r and \n
+    */
+    raw.c_iflag &= ~(ICRNL |IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); // apply modified settings
+}
+int main() {
+    enableRawMode();
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+        if (iscntrl(c)) {
+        printf("%d\r\n", c);
+        } else {
+        printf("%d ('%c')\r\n", c, c);
+        }
     }
-
-    // print file contents
-    std::cout << "--- Text Edit --- ctrl + d to exit\n";
-    std::string line;
-    std::string text;
-    while (std::getline(file, line)){
-        //std::cout << line << std::endl;
-        text += line + "\n";
-    }
-
-    
-    // append more text to the string
-    while(std::getline(std::cin,line)){
-        text += line + "\n";
-    }
-
-    file.close();
     return 0;
 }
+
+
+
+/* Notes
+
+crtl + x is code 24
+
+*/
